ce1be8aaad0da8bd959924950e7a3ab2750fc375
Yuske Fukuyama <yuskefuku@gmail.com>
2017-10-29 23:34:08 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'My Name',
+    'url': '',
+    'download_url': '',
+    'author_email': '',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
+
